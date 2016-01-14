e317ec8fc3a74c334cc373405d118207ad7fc36f
Toan Vo <toan@tinypulse.com>
2016-01-14 20:46:35 +0700

    milestone 3

--- app/bot/language_bot.rb
+++ app/bot/language_bot.rb
@@ -1,7 +1,14 @@
 class LanguageBot < SlackRubyBot::Bot
   command 'hello' do |client, data, match|
-    I18n.locale = :vi
+    I18n.locale = User.find_by_username(data.user).locale
     text = I18n.t 'hello', user: data.user
     client.say(text: text, channel: data.channel)
+  end
+
+  command 'locale' do |client, data, match|
+    user = User.find_or_create_by(username: data.user)
+    user.locale = match[:expression]
+    user.save
+    client.say(text: I18n.t('locale_saved', user.locale), channel: data.channel)
   end
 end
